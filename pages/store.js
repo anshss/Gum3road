bdda6a8472362335500e7fe79ad917bdaac06fe8
Ansh <anshsaxena4190@gmail.com>
2022-07-18 01:26:17 +0530

    updated files

--- pages/store.js
+++ pages/store.js
@@ -1,11 +1,11 @@
-import styles from '../styles/store.module.scss'
-import Navbar from '../components/Navbar'
+// import styles from '../styles/store.module.scss'
+import Navbar from "../components/Navbar";
 
-export default function Store(){
-    return(
-        <div className={styles.store}>
+export default function Store() {
+    return (
+        <>
             <Navbar />
-            store
-        </div>
-    )
+            <div className={styles.store}>store</div>
+        </>
+    );
 }
